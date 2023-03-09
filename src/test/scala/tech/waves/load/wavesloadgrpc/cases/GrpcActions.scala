c759825db67fc1d4ca128686533cc171e665f592
vnikolaenko <vnikolaenko@web3tech.ru>
2023-03-09 10:44:10 +0300

    create based GrpcAction

---
+++ src/test/scala/tech/waves/load/wavesloadgrpc/cases/GrpcActions.scala
@@ -0,0 +1,11 @@
+package tech.waves.load.wavesloadgrpc.cases
+
+import com.github.phisgr.gatling.grpc.Predef.grpc
+import com.github.phisgr.gatling.grpc.action.GrpcCallActionBuilder
+
+import java.awt.Taskbar.Feature
+import scala.swing.Point
+
+object GrpcActions {
+  // val getFeature: GrpcCallActionBuilder[Point, Feature] = grpc()
+}
