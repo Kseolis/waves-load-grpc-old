0517df7dc72c740d6ee1017e17baa817207e9838
vnikolaenko <vnikolaenko@web3tech.ru>
2023-03-10 15:04:47 +0300

    Added logger for logback.xml
    edited setUp for grpc Protocol from mygrpc

--- src/test/scala/tech/waves/load/wavesloadgrpc/Debug.scala
+++ src/test/scala/tech/waves/load/wavesloadgrpc/Debug.scala
@@ -1,17 +1,23 @@
 package tech.waves.load.wavesloadgrpc
 
-import io.gatling.http.Predef._
 import io.gatling.core.Predef._
 import ru.tinkoff.gatling.config.SimulationConfig._
+import tech.waves.load.mygrpc.grpcProtocol
 import tech.waves.load.wavesloadgrpc.scenarios._
 
 class Debug extends Simulation {
 
   setUp(
+    new CommonScenario().grpc.inject(constantUsersPerSec(100) during 15),
+  )
+    .protocols(grpcProtocol)
+    .maxDuration(testDuration)
+
+/*  setUp(
     // default = inject(atOnceUsers(1))
     HttpScenario().inject(constantUsersPerSec(1) during 1),
-    new CommonScenario().grpc.inject(constantUsersPerSec(100) during 15),
+
   ).protocols(
     httpProtocol.proxy(Proxy("devnet1-htz-nbg1-1.wavesnodes.com", 6869).httpsPort(6869)),
-  ).maxDuration(testDuration)
+  ).maxDuration(testDuration)*/
 }
