3f44b027addf5193519572c6718717a78cb2dc3d
vnikolaenko <vnikolaenko@web3tech.ru>
2023-03-10 14:14:52 +0300

    edited Base58 to Base64

--- src/test/scala/tech/waves/load/wavesloadgrpc/cases/GrpcActions.scala
+++ src/test/scala/tech/waves/load/wavesloadgrpc/cases/GrpcActions.scala
@@ -4,13 +4,14 @@ import com.github.phisgr.gatling.grpc.Predef._
 import com.github.phisgr.gatling.grpc.action.GrpcCallActionBuilder
 import com.google.protobuf.ByteString
 import com.wavesplatform.api.grpc.{AssetInfoResponse, AssetRequest, AssetsApiGrpc}
-import com.wavesplatform.crypto.base.Base58
+import com.wavesplatform.crypto.base.Base64
 import io.gatling.core.Predef._
 import io.gatling.core.session.Expression
 import io.grpc.Status
 
+
 object GrpcActions {
-  val assetId: Array[Byte] = Base58.decode("aGMxZMlRCKPavshL7mtimuBH0jKIiMsy7+FD1ACTwW0=")
+  val assetId: Array[Byte] = Base64.decode("aGMxZMlRCKPavshL7mtimuBH0jKIiMsy7+FD1ACTwW0=")
 
   val message: Expression[AssetRequest] = AssetRequest.of(ByteString.copyFrom(assetId))
 
