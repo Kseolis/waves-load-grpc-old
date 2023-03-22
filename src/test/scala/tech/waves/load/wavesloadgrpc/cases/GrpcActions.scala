d0023962d61040cc7fdfcb2fe5fd390ec101b274
vnikolaenko <vnikolaenko@web3tech.ru>
2023-03-22 19:03:53 +0300

    added new assetId for assetsId.csv edited Feeders.scala

--- src/test/scala/tech/waves/load/wavesloadgrpc/cases/GrpcActions.scala
+++ src/test/scala/tech/waves/load/wavesloadgrpc/cases/GrpcActions.scala
@@ -4,21 +4,16 @@ import com.github.phisgr.gatling.grpc.Predef._
 import com.github.phisgr.gatling.grpc.action.GrpcCallActionBuilder
 import com.google.protobuf.ByteString
 import com.wavesplatform.api.grpc.{AssetInfoResponse, AssetRequest, AssetsApiGrpc}
-import com.wavesplatform.crypto.base.Base64
 import io.gatling.core.Predef._
-import io.gatling.core.session.Expression
 import io.grpc.Status
+import tech.waves.load.wavesloadgrpc.feeders.Feeders.feeder
 
 object GrpcActions {
-  val assetId: Array[Byte] = Base64.decode("aGMxZMlRCKPavshL7mtimuBH0jKIiMsy7+FD1ACTwW0=")
-
-  val message: Expression[AssetRequest] = AssetRequest.of(ByteString.copyFrom(assetId))
-
   val getFeature: GrpcCallActionBuilder[AssetRequest, AssetInfoResponse] =
     grpc("AssetsApi")
       .rpc(AssetsApiGrpc.METHOD_GET_INFO)
       .payload {
-        "#{assetId}"
+        AssetRequest.of(ByteString.copyFrom(feeder.next()("assetId")))
       }
       .check(statusCode is Status.Code.OK)
 }
--- src/test/scala/tech/waves/load/wavesloadgrpc/feeders/Feeders.scala
+++ src/test/scala/tech/waves/load/wavesloadgrpc/feeders/Feeders.scala
@@ -1,6 +1,5 @@
 package tech.waves.load.wavesloadgrpc.feeders
 
-import com.google.protobuf.ByteString
 import com.wavesplatform.crypto.base.Base58
 import io.gatling.core.Predef.{configuration, csv}
 import io.gatling.core.feeder.Feeder
@@ -11,12 +10,11 @@ import scala.util.Random
 object Feeders {
   val introducedDate: Feeder[String] = RandomDateFeeder("introduced")
 
-  val assetsIdCsvFeeder: Seq[ByteString] = csv("assetsId.csv").readRecords
+  val assetsIdCsvFeeder: Seq[Array[Byte]] = csv("assetsId.csv").readRecords
     .map(value => {
-      ByteString.copyFrom(Base58.decode(value("assetId").toString))
+      Base58.decode(value("assetId").toString)
     })
 
-
-  lazy val feeder: Iterator[Map[String, ByteString]] =
+  lazy val feeder: Iterator[Map[String, Array[Byte]]] =
     Iterator.continually(Map("assetId" -> assetsIdCsvFeeder(Random.nextInt(assetsIdCsvFeeder.length))))
 }
